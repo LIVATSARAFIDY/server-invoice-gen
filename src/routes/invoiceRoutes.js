265f6ddc84b55e78fa15f8e82aa4c1270699318c
tsiox.dev <tsiox.dev@gmail.com>
2025-07-20 21:16:02 +0300

    simple commit3

--- src/routes/invoiceRoutes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const invoiceController_1 = require("../controllers/invoiceController");
-const router = (0, express_1.Router)();
-router.get('/default-currencies', invoiceController_1.defaultCurrencies);
-exports.default = router;
